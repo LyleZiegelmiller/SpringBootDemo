b4ad2f570202114eda1d08df5eabca3952ac7eec
Lyle Ziegelmiller <lyle_z@hotmail.com>
2020-10-22 16:37:07 +0100

    Fixed up pom.xml

--- JavaBasedAnnotation/src/main/java/JavaBasedAnnotationApplication.java
+++ JavaBasedAnnotation/src/main/java/JavaBasedAnnotationApplication.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavaBasedAnnotationApplication {
