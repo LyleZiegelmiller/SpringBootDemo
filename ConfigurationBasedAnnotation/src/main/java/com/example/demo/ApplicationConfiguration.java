0abdfdae1acf7deaa528c7bbd46b42efc9a889c4
Lyle Ziegelmiller <lyle_z@hotmail.com>
2020-10-27 09:28:16 +0000

    making progress

--- ConfigurationBasedAnnotation/src/main/java/com/example/demo/ApplicationConfiguration.java
+++ ConfigurationBasedAnnotation/src/main/java/com/example/demo/ApplicationConfiguration.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("file:config/local.properties")
 public class ApplicationConfiguration
 {
-
+    @Autowired
+    Environment env;
+    
+    @Bean
+    public TestBean testBean() {
+        TestBean testBean = new TestBean();
+        String value = env.getProperty("key1");
+        testBean.setName(value);
+        return testBean;
+    }    
+    
 }
--- ConfigurationBasedAnnotation/src/main/java/com/example/demo/ConfigurationBasedAnnotationApplication.java
+++ ConfigurationBasedAnnotation/src/main/java/com/example/demo/ConfigurationBasedAnnotationApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigurationBasedAnnotationApplication {
 
+    TestBean testBean;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationBasedAnnotationApplication.class, args);
+		
+		ConfigurationBasedAnnotationApplication obj = new ConfigurationBasedAnnotationApplication();
+		obj.printProperty();
 	}
-
+	
+	void printProperty() {
+	    System.out.println("testBean.getProperty () = " +  testBean.getProperty ());
+	}
+	
+	void setProperty ( @Autowired TestBean testBean) {
+        this.testBean = testBean;
+	}
 }
