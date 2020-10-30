27e7de09e4eefe9dccac90c21fff8ef6eea3da17
Lyle Ziegelmiller <lyle_z@hotmail.com>
2020-10-30 15:40:58 +0000

    Removed unneeded @PropertySource annotation

--- JavaBasedAnnotation/src/main/java/MyInteger.java
+++ JavaBasedAnnotation/src/main/java/MyInteger.java
@@ -3,10 +3,8 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("file:src/main/resources/application.properties")
 public class MyInteger
 {
     private int i;
