dc6efd0b110f72afae76ac05d5f3116ee52cd542
Lyle Ziegelmiller <lyle_z@hotmail.com>
2020-10-31 10:14:06 +0000

    Now using @PropertySource for file src/main/resources/app.properties

--- JavaBasedAnnotation/src/main/java/MyInteger.java
+++ JavaBasedAnnotation/src/main/java/MyInteger.java
@@ -1,12 +1,15 @@
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("file:src/main/resources/app.properties")
 public class MyInteger
 {
     private int i;
     
 //    @Autowired
+
     public MyInteger (@Value ("${myInt}") Integer i)
     {
         this.i = i.intValue ();
