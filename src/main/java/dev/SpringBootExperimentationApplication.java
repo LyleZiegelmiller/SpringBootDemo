6781b99b5e2d218aded9aa21f1376ca7ca46cbf2
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-02-06 13:13:52 -0800

    Created new Spring Boot project and migrated code to it

---
+++ src/main/java/dev/SpringBootExperimentationApplication.java
@@ -0,0 +1,13 @@
+package dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExperimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExperimentationApplication.class, args);
+	}
+
+}
