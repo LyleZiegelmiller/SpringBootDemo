0cb2711aff533804808ac8c316813424bd6bcbd4
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-01 16:26:50 -0800

    Created Actuators module

---
+++ Actuators/src/main/java/application/ActuatorApplication.java
@@ -0,0 +1,16 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication
+{
+    public static void main ( String [] args )
+    {
+        // TODO Auto-generated method stub
+        SpringApplication.run ( ActuatorApplication.class, args );
+
+    }
+
+}
