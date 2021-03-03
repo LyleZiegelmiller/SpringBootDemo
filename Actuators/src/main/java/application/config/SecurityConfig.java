239308bd687c09177da33d1802fed0c43d272b67
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-02 16:03:27 -0800

    Nothing is working

--- Actuators/src/main/java/application/config/SecurityConfig.java
+++ Actuators/src/main/java/application/config/SecurityConfig.java
@@ -8,33 +8,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-          auth.inMemoryAuthentication()
-          .withUser("user").password("{noop}password").roles("USER") .and()
-          .withUser("admin").password("{noop}password").roles("ADMIN");
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http 
-            .csrf() 
-            .disable()
-            .authorizeRequests()
-            .antMatchers("/", "/home","/guest").permitAll() 
-            .antMatchers("/admin/**").hasAnyRole("ADMIN")
-            .antMatchers("/user/**").hasAnyRole("USER")
-            .anyRequest().authenticated();
-    }
-}
-
 //@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter
-//{
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//          auth.inMemoryAuthentication()
+//          .withUser("user").password("{noop}password").roles("USER") .and()
+//          .withUser("admin").password("{noop}password").roles("ADMIN");
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http 
+//            .csrf() 
+//            .disable()
+//            .authorizeRequests()
+//            .antMatchers("/", "/home","/guest").permitAll() 
+//            .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//            .antMatchers("/user/**").hasAnyRole("USER")
+//            .anyRequest().authenticated();
+//    }
+//}
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        PasswordEncoder encoder = 
@@ -49,14 +49,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //          .password(encoder.encode("password123"))
 //          .roles("USER", "ADMIN", "ACTUATOR");
 //    }   
-//    
-//    @Override
-//    protected void configure ( HttpSecurity http ) throws Exception
-//    {
-//        http.authorizeRequests ().mvcMatchers ( "/actuator/health", "actuator/info" ).permitAll ()
-//                                                .mvcMatchers ( "/actuator/**" )
-//                                                .hasRole ( "ACTUATOR" ).anyRequest ()
-//                                                .authenticated ();
-//    }
-//
-//}
+    
+    @Override
+    protected void configure ( HttpSecurity http ) throws Exception
+    {
+        http.authorizeRequests ().mvcMatchers ( "/actuator/health", "actuator/info" ).permitAll ()
+                                                .mvcMatchers ( "/actuator/**" )
+                                                //.hasRole ( "ACTUATOR" )
+                                                //.anyRequest ()
+                                                .authenticated ();
+    }
+}
