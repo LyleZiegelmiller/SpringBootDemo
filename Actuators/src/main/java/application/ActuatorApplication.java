00069b7678978aaf7434f0fb604fa6486a7202bd
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-02 16:07:45 -0800

    Reverting to 0cb2711aff533804808ac8c316813424bd6bcbd4

--- Actuators/src/main/java/application/ActuatorApplication.java
+++ Actuators/src/main/java/application/ActuatorApplication.java
@@ -2,9 +2,8 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication //(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class ActuatorApplication
 {
     public static void main ( String [] args )
