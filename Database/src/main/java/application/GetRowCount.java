4a05573f0298ef859d20846d8c485eb36e7475ca
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-05 13:04:31 -0800

    Added /accounts/totalNumber RestController endpoint

--- Database/src/main/java/application/GetRowCount.java
+++ Database/src/main/java/application/GetRowCount.java
@@ -3,11 +3,12 @@ package application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component
+@RestController
 public class GetRowCount
 {
     protected final Logger logger;
@@ -20,10 +21,13 @@ public class GetRowCount
         logger = LoggerFactory.getLogger(getClass());
     }
 
-    public void testNumberOfAccount() throws Exception {
+    @GetMapping("/accounts/totalNumber")
+    public long testNumberOfAccount() throws Exception {
 
         long count = jdbcTemplate.queryForObject(QUERY, Long.class);
 
         logger.info ( "ljz_cust_data has " + count + " rows." );
+        
+        return count;
     }
 }
