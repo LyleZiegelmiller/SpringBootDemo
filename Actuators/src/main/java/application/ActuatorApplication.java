2e854f9f9d0183e73467c88d927ac7eade2a6ee1
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-08 10:28:05 -0800

    Removed lines from application-dev.properties that failed to disable the login requirement. Uncommented the lines which actually achieve this in two other class files.

--- Actuators/src/main/java/application/ActuatorApplication.java
+++ Actuators/src/main/java/application/ActuatorApplication.java
@@ -3,10 +3,13 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(exclude = {
-//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-//        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-//        )
+/*
+ * The exclude below suppresses Spring Boot Security
+ */
+@SpringBootApplication(scanBasePackages = {"configure", "application"}, exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+        )
 @SpringBootApplication
 public class ActuatorApplication
 {
--- Database/src/main/java/application/DatabaseApplication.java
+++ Database/src/main/java/application/DatabaseApplication.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
         )
-//@SpringBootApplication(scanBasePackages = {"configure", "application"})
 public class DatabaseApplication
 {
     private final Logger logger = LoggerFactory.getLogger(DatabaseApplication.class);
