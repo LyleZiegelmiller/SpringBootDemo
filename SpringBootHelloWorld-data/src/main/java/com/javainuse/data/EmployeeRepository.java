20e27dc9da0cf3b318c4e64cd60b2b0af7d118bd
Lyle Ziegelmiller <lyle_z@hotmail.com>
2021-03-28 13:58:58 -0700

    Deleted SpringBootHelloworld-data

--- SpringBootHelloWorld-data/src/main/java/com/javainuse/data/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javainuse.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javainuse.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
